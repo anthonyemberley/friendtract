d1fdf8c8f47990946b8787d7b29b792e7eb0558e
Harrison Wang <harrison.wang@appboy.com>
2015-12-07 21:36:34 -0500

    groupme stuff

--- client/myapp.js
+++ client/myapp.js
@@ -30,40 +30,12 @@ if (Meteor.isClient) {
 
   Template.set_value.events({
     'click button': function () {
-      var API = require('groupme').Stateless;
-      const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
-      var value = parseInt($("input.text").val(), 0);
+      var value = parseInt($("input.text").val(),0);
       SimpleStorage.set(value);
       addToLog("SimpleStorage.set("+value+")");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  console.log("here");
-  Meteor.startup(function () {
-    //var index = require('./index.js');
-    var API = require('groupme').Stateless;
-    const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
-    const group_id = "18335909";
-    API.Users.me(ACCESS_TOKEN, function(err,ret) {
-      if (!err) {
-        console.log("Your user id is", ret.id, "and your name is", ret.name);        
-      }
-    });
-
-    
-        API.Messages.index(ACCESS_TOKEN, group_id, [], function(err,ret){
-        if (!err) {
-          console.log(ret.messages);  
-          ret.messages.forEach(function(message){
-            if(message.sender_id == '11386760'){
-              console.log("we did it");
-              //SimpleStorage.set(101);
-              //gotText();
-            }
-          })      
-        }
+      Meteor.call('groupmeTest',function(err,res){
+        console.log(res);
       })
+    }
   });
 }
--- server/groupme.js
+++ server/groupme.js
@@ -1,24 +1,34 @@
 
-//var index = require('./index.js');
-// var API = require('groupme').Stateless;
-// const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
-// const group_id = "18335909";
-// API.Users.me(ACCESS_TOKEN, function(err,ret) {
-//   if (!err) {
-//     console.log("Your user id is", ret.id, "and your name is", ret.name);        
-//   }
-// });
+Meteor.methods({
+  groupmeTest: function (post) {
+    var API = Meteor.npmRequire('groupme').Stateless;
+    const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
+    const group_id = "18335909";
+    API.Users.me(ACCESS_TOKEN, function(err,ret) {
+      if (!err) {
+        console.log("Your user id is", ret.id, "and your name is", ret.name);
+      }
+      console.log(err);
+    });
 
-    
-// API.Messages.index(ACCESS_TOKEN, group_id, [], function(err,ret){
-//   if (!err) {
-//     console.log(ret.messages);  
-//     ret.messages.forEach(function(message){
-//       if(message.sender_id == '11386760'){
-//         console.log("we did it");
-//         //SimpleStorage.set(101);
-//         //gotText();
-//       }
-//     })      
-//   }
-// })
+    API.Messages.index(ACCESS_TOKEN, group_id, [], function(err,ret){
+      if (!err) {
+        ret.messages.forEach(function(message){
+          if(message.sender_id == '11386760'){
+            console.log("we did it");
+            //SimpleStorage.set(101);
+            //gotText();
+          }
+        })
+      }
+    });
+
+    return ACCESS_TOKEN;
+  }
+
+})
+
+
+
+
+

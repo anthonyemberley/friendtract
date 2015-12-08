5ed2f4393bcf6cf20707ed62d5f05d119b35b53d
Harrison Wang <harrison.wang@appboy.com>
2015-12-07 22:40:25 -0500

    added semantic ui

--- server/groupme.js
+++ server/groupme.js
@@ -1,25 +1,28 @@
 
 Meteor.methods({
-  groupmeTest: function (post) {
+  groupmeMagic: function (data) {
     var API = Meteor.npmRequire('groupme').Stateless;
     const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
     const group_id = "18335909";
+    console.log(data);
     API.Users.me(ACCESS_TOKEN, function(err,ret) {
-      if (!err) {
-        console.log("Your user id is", ret.id, "and your name is", ret.name);
+      if (err) {
+        throw err;
       }
-      console.log(err);
+      //console.log("Your user id is", ret.id, "and your name is", ret.name);
     });
 
     API.Messages.index(ACCESS_TOKEN, group_id, [], function(err,ret){
       if (!err) {
         ret.messages.forEach(function(message){
           if(message.sender_id == '11386760'){
-            console.log("we did it");
             //SimpleStorage.set(101);
             //gotText();
           }
         })
+      }
+      else{
+        throw err;
       }
     });
 

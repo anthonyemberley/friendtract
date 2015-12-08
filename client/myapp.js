78db08f95783467264164b6c6823ec18afb27f04
kerhardt <kerhardt@mit.edu>
2015-12-07 20:44:38 -0500

    add .meteor/

--- client/myapp.js
+++ client/myapp.js
@@ -38,7 +38,30 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  console.log("here");
   Meteor.startup(function () {
-    // code to run on server at startup
+    //var index = require('./index.js');
+    var API = require('groupme').Stateless;
+    const ACCESS_TOKEN = "f487ce307f3601338c7306fd38831e3b";
+    const group_id = "18335909";
+    API.Users.me(ACCESS_TOKEN, function(err,ret) {
+      if (!err) {
+        console.log("Your user id is", ret.id, "and your name is", ret.name);        
+      }
+    });
+
+    
+        API.Messages.index(ACCESS_TOKEN, group_id, [], function(err,ret){
+        if (!err) {
+          console.log(ret.messages);  
+          ret.messages.forEach(function(message){
+            if(message.sender_id == '11386760'){
+              console.log("we did it");
+              //SimpleStorage.set(101);
+              //gotText();
+            }
+          })      
+        }
+      })
   });
 }
